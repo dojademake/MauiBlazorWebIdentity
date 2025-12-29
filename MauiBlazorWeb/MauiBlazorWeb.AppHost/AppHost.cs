312952a16f29012d796a9a64edd5cf43f4d03c17
Doug Dekker <doug.dekker@mission-llc.com>
2025-12-29 15:50:00 -0500

    Add swagger link to Aspire resources

--- MauiBlazorWeb/MauiBlazorWeb.AppHost/AppHost.cs
+++ MauiBlazorWeb/MauiBlazorWeb.AppHost/AppHost.cs
@@ -1,5 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.MauiBlazorWeb_Web>("mauiblazorweb-web");
+var apiService = builder.AddProject<Projects.MauiBlazorWeb_Web>("apiservice");
+
+apiService.WithUrlForEndpoint("https", url =>
+{
+    url.DisplayText = "MauiBlazorWeb API Swagger UI";
+    url.Url = "/swagger";
+});
+
+builder.AddProject<Projects.MauiBlazorWeb_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService)
+    .WaitFor(apiService);
 
 builder.Build().Run();
